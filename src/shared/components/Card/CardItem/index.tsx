14c60d9ce4da0ef7fd891203240cc670b6db6821
Tiago Borges <tiagoborges899@gmail.com>
2024-10-26 19:55:45 -0300

    feat: criação do CardItem Component

---
+++ src/shared/components/Card/CardItem/index.tsx
@@ -0,0 +1,10 @@
+export const CardItem = () => {
+    return (
+        <li className="card__list__item">
+            <img src="" alt="" />
+            <div className="card__list__item__description">
+                {/* MOVIE DETAILS */}
+            </div>
+        </li>
+    )
+}
