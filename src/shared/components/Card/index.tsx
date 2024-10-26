794fbc465e94d79ce55c6d6729df3af056bf10a3
Tiago Borges <tiagoborges899@gmail.com>
2024-10-26 19:52:49 -0300

    refactor: mudança do nome do component ProductCard para Card

---
+++ src/shared/components/Card/index.tsx
@@ -0,0 +1,7 @@
+export const Card = () => {
+    return (
+        <ul className="card__list">
+            {/* ProductCardItem */}
+        </ul>
+    )
+}
--- src/shared/components/ProductCard/index.tsx
+++
@@ -1,7 +0,0 @@
-export const ProductCard = () => {
-    return (
-        <ul className="product__card__list">
-            {/* ProductCardItem */}
-        </ul>
-    )
-}
