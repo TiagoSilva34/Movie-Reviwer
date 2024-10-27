56729a0c1eeb8bd11c2c70b476bdc99e42c5b314
Tiago Borges <tiagoborges899@gmail.com>
2024-10-27 14:39:33 -0300

    feat: criação do Header component

---
+++ src/shared/components/Header/index.tsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__logo">Logo Aqui</h1>
+
+            <form name="header__search__form">
+                
+            </form>
+
+            <div className="header__account">
+                <div className="header__account__avatar">
+                    <img src="" alt="Avatar" />
+                </div>
+                <p>Nome do usuário</p>
+            </div>
+
+            {/* NAVBAR MOBILE ICON BUTTON */}
+        </header>
+    )
+}
